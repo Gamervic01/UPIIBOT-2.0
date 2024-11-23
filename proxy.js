116fa7c8f16fb5561c8376e5c65bb96104b24c5f
Víctor Manuel López Pérez <72628195+Gamervic01@users.noreply.github.com>
2024-11-22 20:42:45 -0600

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -15,6 +15,7 @@ app.get('/proxy', async (req, res) => {
   }
 
   try {
+    console.log(`Proxying request to: ${url}`);
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: 'domcontentloaded' });
@@ -25,9 +26,13 @@ app.get('/proxy', async (req, res) => {
     res.send(content);
   } catch (error) {
     console.error('Error en el proxy con Puppeteer:', error.message);
-    res.status(500).json({ error: 'Error al obtener la URL con Puppeteer' });
+    res.status(500).json({
+      error: 'Error al obtener la URL con Puppeteer',
+      details: error.message,
+    });
   }
 });
+
 
 app.listen(PORT, () => {
   console.log(`Proxy escuchando en http://localhost:${PORT}`);
